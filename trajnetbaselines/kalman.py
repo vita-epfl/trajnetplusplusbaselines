2aae9f808860f4c11625ef0b62bd21a8b59e26e4
theDebugger811 <parthkothari811@gmail.com>
2019-09-06 12:57:08 +0200

    Kalman updated for primary

--- trajnetbaselines/kalman.py
+++ trajnetbaselines/kalman.py
@@ -3,9 +3,14 @@ import pykalman
 import trajnettools
 
 
-def predict(paths):
+def predict(paths, predict_all=False):
     multimodal_outputs = {}
     neighbours_tracks = []
+
+    ## Primary Prediction
+    if not predict_all:
+        paths = paths[0:1]
+    
     for i, path in enumerate(paths):
         path = paths[i]
         initial_state_mean = [path[0].x, 0, path[0].y, 0]
@@ -30,8 +35,8 @@ def predict(paths):
 
         # prepare predictions
         frame_diff = path[1].frame - path[0].frame
-        first_frame = path[-1].frame + frame_diff
-        ped_id = path[-1].pedestrian
+        first_frame = path[8].frame + frame_diff
+        ped_id = path[8].pedestrian
 
         # sample predictions (first sample corresponds to last state)
         # average 5 sampled predictions
@@ -43,11 +48,15 @@ def predict(paths):
             else:
                 predictions += pred
         predictions /= 5.0
+
+        #write
         if i == 0:
             primary_track = [trajnettools.TrackRow(first_frame + j * frame_diff, ped_id, x, y)
             for j, (x, y) in enumerate(predictions[1:])]
         else:
             neighbours_tracks.append([trajnettools.TrackRow(first_frame + j * frame_diff, ped_id, x, y)
             for j, (x, y) in enumerate(predictions[1:])])
+
+    ## Unimodal Ouput
     multimodal_outputs[0] = primary_track, neighbours_tracks
     return multimodal_outputs
