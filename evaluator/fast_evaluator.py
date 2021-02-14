f7f11e1cf0d748f716d479b586e4c167457ed1d4
theDebugger811 <parthkothari811@gmail.com>
2021-02-14 10:52:22 +0100

    Evaluator updated

--- evaluator/fast_evaluator.py
+++ evaluator/fast_evaluator.py
@@ -12,14 +12,15 @@ import torch
 import trajnetplusplustools
 import trajnetbaselines
 
+## Parallel Compute
+import multiprocessing
+from joblib import Parallel, delayed
+from tqdm import tqdm
 
-def get_goals(paths, goal_dict, filename, scene_id):
-    ## get goals
-    if len(goal_dict):
-        scene_goal = np.array(goal_dict[filename][scene_id])
-    else:
-        scene_goal = np.array([[0, 0] for path in paths])
-    return scene_goal
+def process_scene(predictor, model_name, paths, scene_goal, args):
+    ## For each scene, get predictions
+    predictions = predictor(paths, scene_goal, n_predict=args.pred_length, obs_length=args.obs_length, modes=args.modes, args=args)
+    return predictions
 
 def main():
     parser = argparse.ArgumentParser()
@@ -39,8 +40,6 @@ def main():
                         help='labels of models')
     parser.add_argument('--normalize_scene', action='store_true',
                         help='augment scenes')
-    parser.add_argument('--goals', action='store_true',
-                        help='Considers goals during prediction')
     parser.add_argument('--unimodal', action='store_true',
                         help='provide unimodal evaluation')
     parser.add_argument('--topk', action='store_true',
@@ -49,6 +48,14 @@ def main():
                         help='provide multimodal nll evaluation')
     parser.add_argument('--modes', default=1, type=int,
                         help='number of modes to predict')
+    parser.add_argument('--scene_type', default=0, type=int,
+                        choices=(0, 1, 2, 3, 4),
+                        help='type of scene to evaluate')
+    parser.add_argument('--thresh', default=0.0, type=float,
+                        help='noise thresh')
+    parser.add_argument('--ped_type', default='primary',
+                        help='type of ped to add noise to')
+
     args = parser.parse_args()
 
     scipy.seterr('ignore')
@@ -62,8 +69,18 @@ def main():
     if (not args.unimodal) and (not args.topk) and (not args.multimodal):
         args.unimodal = True # Compute unimodal metrics by default
 
+    if args.topk:
+        args.modes = 3
+
     if args.multimodal:
         args.modes = 20
+
+    enable_col1 = True
+    ## drop pedestrians that appear post observation
+    def drop_post_obs(ground_truth, obs_length):
+        obs_end_frame = ground_truth[0][obs_length].frame
+        ground_truth = [track for track in ground_truth if track[0].frame < obs_end_frame]
+        return ground_truth
 
     ## Writes to Test_pred
     ## Does this overwrite existing predictions? No. ###
@@ -77,9 +94,12 @@ def main():
         print("Model Name: ", model_name)
         if 'sgan' in model_name:
             predictor = trajnetbaselines.sgan.SGANPredictor.load(model)
+            goal_flag = predictor.model.generator.goal_flag
         else:
             predictor = trajnetbaselines.lstm.LSTMPredictor.load(model)
+            goal_flag = predictor.model.goal_flag
 
+        print("Goal Flag:", goal_flag)
         # On CPU
         device = torch.device('cpu')
         predictor.model.to(device)
@@ -105,36 +125,55 @@ def main():
             if 'collision_test' in name:
                 continue
 
+            ## Filter for Scene Type
+            reader_tag = trajnetplusplustools.Reader(args.path.replace('_pred', '_private') + dataset + '.ndjson', scene_type='tags')
+            if args.scene_type != 0:
+                filtered_scene_ids = [s_id for s_id, tag, s in reader_tag.scenes() if tag[0] == args.scene_type]
+            else:
+                filtered_scene_ids = [s_id for s_id, _, _ in reader_tag.scenes()]
+
             # Read file from 'test'
             reader = trajnetplusplustools.Reader(args.path.replace('_pred', '') + dataset + '.ndjson', scene_type='paths')
             ## Necessary modification of train scene to add filename (for goals)
-            scenes = [(dataset, s_id, s) for s_id, s in reader.scenes()]
+            scenes = [(dataset, s_id, s) for s_id, s in reader.scenes() if s_id in filtered_scene_ids]
+
             ## Consider goals
             ## Goal file must be present in 'goal_files/test_private' folder 
             ## Goal file must have the same name as corresponding test file 
-            if args.goals:
+            if goal_flag:
                 goal_dict = pickle.load(open('goal_files/test_private/' + dataset +'.pkl', "rb"))
                 all_goals[dataset] = {s_id: [goal_dict[path[0].pedestrian] for path in s] for _, s_id, s in scenes}
 
+            ## Get Goals
+            if goal_flag:
+                scene_goals = [np.array(all_goals[filename][scene_id]) for filename, scene_id, _ in scenes]
+            else:
+                scene_goals = [np.zeros((len(paths), 2)) for _, scene_id, paths in scenes]
+
+            print("Getting Predictions")
+            scenes = tqdm(scenes)
+            ## Get all predictions in parallel. Faster!
+            pred_list = Parallel(n_jobs=12)(delayed(process_scene)(predictor, model_name, paths, scene_goal, args)
+                                            for (_, _, paths), scene_goal in zip(scenes, scene_goals))
+
+            ## GT Scenes
             reader_gt = trajnetplusplustools.Reader(args.path.replace('_pred', '_private') + dataset + '.ndjson', scene_type='paths')
-            scenes_gt = [s for _, s in reader_gt.scenes()]
+            scenes_gt = [s for s_id, s in reader_gt.scenes() if s_id in filtered_scene_ids]
             total_scenes += len(scenes_gt)
 
-            for i, (filename, scene_id, paths) in enumerate(scenes):
-                if i % 100 == 0:
-                    print("Scenes evaluated: ", '{}/{}'.format(i, len(scenes_gt)))
-                ground_truth = scenes_gt[i]
+            print("Evaluating Predictions")
+            scenes = tqdm(scenes)
+            for (predictions, (_, scene_id, paths), ground_truth) in zip(pred_list, scenes, scenes_gt):
 
-                ## Convert numpy array to Track Rows ##
                 ## Extract 1) first_frame, 2) frame_diff 3) ped_ids for writing predictions
                 observed_path = paths[0]
                 frame_diff = observed_path[1].frame - observed_path[0].frame
                 first_frame = observed_path[args.obs_length-1].frame + frame_diff
                 ped_id = observed_path[0].pedestrian
-
-                goals = get_goals(paths, all_goals, filename, scene_id) ## Zeros if no goals utilized
-                predictions = predictor(paths, goals, n_predict=args.pred_length, obs_length=args.obs_length, modes=args.modes, args=args)
-
+                ped_id_ = []
+                for j, _ in enumerate(paths[1:]): ## Only need neighbour ids
+                    ped_id_.append(paths[j+1][0].pedestrian)
+                
                 if args.unimodal: ## Unimodal
                     ## ADE / FDE
                     prediction, neigh_predictions = predictions[0]
@@ -159,14 +198,19 @@ def main():
                     average += average_l2
                     final += final_l2
 
+                    ground_truth = drop_post_obs(ground_truth, args.obs_length)
                     ## Collision Metrics
                     for j in range(1, len(ground_truth)):
                         if trajnetplusplustools.metrics.collision(primary_tracks, ground_truth[j], n_predictions=args.pred_length):
                             gt_col += 1
                             break
 
-                    # ## neighbours (if not empty) [Col-I]
-                    if neigh_predictions.shape[1]:
+                    num_gt_neigh = len(ground_truth) - 1
+                    num_predicted_neigh = neigh_predictions.shape[1]
+                    if num_gt_neigh != num_predicted_neigh:
+                        enable_col1 = False
+                    # [Col-I] only if neighs in gt = neighs in prediction
+                    if enable_col1:
                         neigh_scenes += 1
                         for n in range(neigh_predictions.shape[1]):
                             neigh = neigh_predictions[:, n]
@@ -194,15 +238,15 @@ def main():
             average /= total_scenes
             final /= total_scenes
             gt_col /= (total_scenes * 0.01)
-            if neigh_scenes == 0:
+            if not enable_col1:
                 pred_col = -1
             else:
                 pred_col /= (neigh_scenes * 0.01)
 
-            print('ADE: ', average)
-            print('FDE: ', final)
-            print("Col-I: ", pred_col)
-            print("Col-II: ", gt_col)
+            print('ADE: ', np.round(average, 3))
+            print('FDE: ', np.round(final, 3))
+            print("Col-I: ", np.round(pred_col, 2))
+            print("Col-II: ", np.round(gt_col, 2))
 
         if args.topk:
             topk_average /= total_scenes
